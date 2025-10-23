feb73f960eedac33a063e77cea3c2f5c52b01542
rjaymf10 <rjaymf10@gmail.com>
2025-10-23 23:34:34 +0800

    Tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,18 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './providers/theme/ThemeProvider.tsx'
 import NavigationProvider from './providers/navigation/NavigationProvider.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <NavigationProvider>
-        <App />
-      </NavigationProvider>
+      <QueryClientProvider client={queryClient}>
+        <NavigationProvider>
+          <App />
+        </NavigationProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 )
--- frontend/src/views/WalletView.tsx
+++ frontend/src/views/WalletView.tsx
@@ -1,4 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
 const WalletView = () => {
+  const { isPending, isFetching, data, error } = useQuery({
+    queryKey: ["walletInfo"],
+    queryFn: async () => {
+      return await fetch("https://api.github.com/repos/TanStack/query").then(async (res) => {
+        return await res.json();
+      });
+    }
+  });
+
+  if (isPending) return <div>Loading...</div>;
+  if (error) return <div>Error loading wallet info</div>;
 
   return (
     <>
@@ -6,7 +19,9 @@ const WalletView = () => {
         <h1 className="text-4xl font-bold">Your Wallet Info</h1>
       </div>
       <div>
-        Wallet View
+        <h2 className="mb-4 text-2xl font-semibold">{data.full_name}</h2>
+        <p className="mb-2">{data.description}</p>
+        <div>{isFetching ? "Updating" : ""}</div>
       </div>
     </>
   )
