0463d4d6ca16b7ab421293c430528bd71d004da0
rjaymf10 <rjaymf10@gmail.com>
2025-10-24 01:27:15 +0800

    sui packages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 import { ThemeProvider } from './providers/theme/ThemeProvider.tsx'
 import NavigationProvider from './providers/navigation/NavigationProvider.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit'
+import { networkConfig } from './config/networkConfig.ts'
 
 const queryClient = new QueryClient()
 
@@ -12,9 +14,13 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
-        <NavigationProvider>
-          <App />
-        </NavigationProvider>
+        <SuiClientProvider defaultNetwork="testnet" networks={networkConfig}>
+          <WalletProvider>
+            <NavigationProvider>
+              <App />
+            </NavigationProvider>
+          </WalletProvider>
+        </SuiClientProvider>
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
